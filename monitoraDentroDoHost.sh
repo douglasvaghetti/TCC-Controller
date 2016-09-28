f30ae8dae56b2b55c78e61ec20813fdd988d179f
Douglas Vaghetti <douglasvaghetti@gmail.com>
2016-09-27 23:30:34 -0700

    tira prints inuteis

--- monitoraDentroDoHost.sh
+++ monitoraDentroDoHost.sh
@@ -11,12 +11,12 @@ do
 
 	for i in $3 #3 eh a lista com os nomes das interfaces do host
 	do
-		echo "rodando na interface $i"
+		#echo "rodando na interface $i"
 		timeout 3s iftop -B -t -n -s 1 -i $i 2>/dev/null | egrep "<=|=>" | ./formatadorSaidaIFtop.py $2 $i >> $arquivo &
-		echo "rodou"
+		#echo "rodou"
 	done
 
-	echo "rodei $arquivo"
+	#echo "rodei $arquivo"
 
 	sleep 3
 done
