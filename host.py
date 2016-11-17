c5e1615a55fb22481194402b5cdda0996cd36110
Hallan Neves <hallanneves@gmail.com>
2016-11-17 04:07:46 -0800

    Separa plot do gráfico de regras e banda.

--- host.py
+++ host.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
@@ -9,11 +11,18 @@ plt.grid(True)
 host = {}
 import os
 execfile("acumulado.py")
-
 eixoX = range(len(dados["malicioso"]))
+
 plt.plot(eixoX, dados['legitimo'], color="blue")
 plt.plot(eixoX, dados['malicioso'], color="red")
-plt.plot(eixoX, dados['regras'], color="green")
+plt.xlabel("Tempo(s)")
+plt.ylabel(u"Tráfego (MB/s)")
+plt.savefig("trafego.png")
+plt.clf()
 
-plt.savefig("test.png")
+plt.plot(eixoX, dados['regras'], color="green")
+plt.xlabel("Tempo(s)")
+plt.ylabel(u"Regras instaladas")
+plt.savefig("regras.png")
+
 #print host
